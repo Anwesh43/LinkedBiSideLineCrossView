832738b459de4a76ff516e956654e412849f1527
Anwesh43 <anweshthecool0@gmail.com>
2019-05-17 04:20:27 +0530

    adding BSLCNode class

--- bisidelinecrossview/src/main/java/com/anwesh/uiprojects/bisidelinecrossview/BiSideLineCrossView.kt
+++ bisidelinecrossview/src/main/java/com/anwesh/uiprojects/bisidelinecrossview/BiSideLineCrossView.kt
@@ -126,4 +126,48 @@ class BiSideLineCrossView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class BSLCNode(var i : Int, val state : State = State()) {
+
+        private var next : BSLCNode? = null
+        private var prev : BSLCNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < nodes - 1) {
+                next = BSLCNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawBSLCNode(i, state.scale, paint)
+            next?.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            state.update {
+                cb(i, it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : BSLCNode {
+            var curr : BSLCNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
